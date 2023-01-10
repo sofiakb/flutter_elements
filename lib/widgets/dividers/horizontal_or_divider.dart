8e403bf86cacef1ddfaeb3fa265c2424d03a64e4
Sofiakb <contact.sofiakb@gmail.com>
2023-01-10 12:27:01 +0100

    Updated -- Add spacing value

--- lib/widgets/dividers/horizontal_or_divider.dart
+++ lib/widgets/dividers/horizontal_or_divider.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 
 class HorizontalOrDivider extends StatelessWidget {
   const HorizontalOrDivider(
-      {this.label = "ou", this.height = 1, this.color = Colors.black});
+      {this.label = "ou", this.height = 1, this.color = Colors.black, this.spacing = 15.0});
 
   final String label;
   final double height;
   final Color color;
+  final double spacing;
 
   @override
   Widget build(BuildContext context) {
     return Row(children: <Widget>[
       Expanded(
         child: new Container(
-            margin: const EdgeInsets.only(left: 10.0, right: 15.0),
+            margin: EdgeInsets.only(left: 10.0, right: spacing),
             child: Divider(
               color: color,
               height: height,
@@ -22,7 +23,7 @@ class HorizontalOrDivider extends StatelessWidget {
       Text(label, style: TextStyle(color: color)),
       Expanded(
         child: new Container(
-            margin: const EdgeInsets.only(left: 15.0, right: 10.0),
+            margin: EdgeInsets.only(left: spacing, right: 10.0),
             child: Divider(
               color: color,
               height: height,
