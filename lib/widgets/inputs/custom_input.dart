57d2504767e4eca565a869f82972230fb8957b01
Sofiakb <contact.sofiakb@gmail.com>
2023-07-11 16:18:21 +0200

    [Updated] autoresize input

--- lib/widgets/inputs/custom_input.dart
+++ lib/widgets/inputs/custom_input.dart
@@ -26,6 +26,7 @@ class CustomInput extends StatefulWidget {
       this.textInputAction,
       this.autocorrect = true,
       this.child,
+      this.resize = false,
       this.inputFormatters})
       : super(key: key);
 
@@ -57,6 +58,8 @@ class CustomInput extends StatefulWidget {
 
   final Widget? child;
 
+  final bool resize;
+
   @override
   State<CustomInput> createState() => _CustomInputState();
 }
@@ -75,83 +78,102 @@ class _CustomInputState extends State<CustomInput> {
     return Padding(
       padding:
           EdgeInsets.only(top: 8.0, bottom: hasErrorMessage() ? 20.0 : 8.0),
-      child: widget.child ??
-          TextFormField(
-            onChanged: widget.onChanged,
-            autocorrect: widget.autocorrect,
-            decoration: InputDecoration(
-              label: widget.title != null
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          if (widget.labelIcon != null)
-                            FaIcon(
-                              widget.labelIcon!,
-                              size: 13.0,
-                            ),
-                          if (widget.labelIcon != null)
-                            SizedBox(
-                              width: 8.0,
-                            ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                widget.title!,
-                              ),
-                              if (widget.required == true)
-                                Text(
-                                  '*',
-                                  style:
-                                      TextStyle(color: Colors.red, fontSize: 8),
-                                ),
-                            ],
-                          ),
-                          // Text("${widget.title}${widget.required == true ? '*' : ''}"),
-                        ],
-                      ),
-                    )
-                  : null,
-              hintText: widget.hintText,
-              prefixIcon: widget.prefixIcon == null
-                  ? null
-                  : Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        widget.prefixIcon!,
-                      ],
-                    ),
-              suffixIcon: widget.suffixIcon == null
-                  ? null
-                  : Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        widget.suffixIcon!,
-                      ],
-                    ),
-            ),
-            autofocus: widget.isFocus,
-            enabled: widget.enabled,
-            controller: widget.controller,
-            obscureText: widget.isObscure,
-            textInputAction: widget.textInputAction,
-            inputFormatters: widget.inputFormatters,
-            validator: (String? value) {
-              return updateErrorMessage(widget.validator != null
-                  ? widget.validator!(value)
-                  : (value == null || value.isEmpty
-                      ? widget.validatorMessage ??
-                          Validator().errorMessage(message: "ce champ")
-                      : null));
-            },
-            keyboardType: widget.textInputType ?? TextInputType.text,
-          ),
+      child: widget.resize
+          ? LayoutBuilder(builder: (context, size) {
+              TextSpan text = new TextSpan(
+                text: widget.controller?.text,
+              );
+
+              TextPainter tp = new TextPainter(
+                text: text,
+                textDirection: TextDirection.ltr,
+                textAlign: TextAlign.left,
+              );
+              tp.layout(maxWidth: size.maxWidth);
+
+              int lines = (tp.size.height / tp.preferredLineHeight).ceil();
+              int maxLines = 10;
+
+              return _widget(maxLines: lines < maxLines ? null : maxLines);
+            })
+          : widget.child ?? _widget(),
     );
   }
+
+  Widget _widget({int? maxLines}) => TextFormField(
+        onChanged: widget.onChanged,
+        autocorrect: widget.autocorrect,
+        decoration: InputDecoration(
+          label: widget.title != null
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      if (widget.labelIcon != null)
+                        FaIcon(
+                          widget.labelIcon!,
+                          size: 13.0,
+                        ),
+                      if (widget.labelIcon != null)
+                        SizedBox(
+                          width: 8.0,
+                        ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.title!,
+                          ),
+                          if (widget.required == true)
+                            Text(
+                              '*',
+                              style: TextStyle(color: Colors.red, fontSize: 8),
+                            ),
+                        ],
+                      ),
+                      // Text("${widget.title}${widget.required == true ? '*' : ''}"),
+                    ],
+                  ),
+                )
+              : null,
+          hintText: widget.hintText,
+          prefixIcon: widget.prefixIcon == null
+              ? null
+              : Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    widget.prefixIcon!,
+                  ],
+                ),
+          suffixIcon: widget.suffixIcon == null
+              ? null
+              : Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    widget.suffixIcon!,
+                  ],
+                ),
+        ),
+        autofocus: widget.isFocus,
+        enabled: widget.enabled,
+        controller: widget.controller,
+        obscureText: widget.isObscure,
+        textInputAction: widget.textInputAction,
+        inputFormatters: widget.inputFormatters,
+        maxLines: maxLines,
+        validator: (String? value) {
+          return updateErrorMessage(widget.validator != null
+              ? widget.validator!(value)
+              : (value == null || value.isEmpty
+                  ? widget.validatorMessage ??
+                      Validator().errorMessage(message: "ce champ")
+                  : null));
+        },
+        keyboardType: widget.textInputType ?? TextInputType.text,
+      );
 
   bool hasErrorMessage() {
     return widget.errorMessage != null &&
