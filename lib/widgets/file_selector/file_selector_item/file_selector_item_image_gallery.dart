f8d89911160505a2157837ce49f969390f043b1d
Sofiakb <contact.sofiakb@gmail.com>
2023-10-15 11:43:34 +0200

    [Updated] add source to item gallery

--- lib/widgets/file_selector/file_selector_item/file_selector_item_image_gallery.dart
+++ lib/widgets/file_selector/file_selector_item/file_selector_item_image_gallery.dart
@@ -2,10 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:sofiakb_elements/widgets/file_selector/file_selector_item/file_selector_item_child.dart';
 
 class FileSelectorItemImageGallery extends FileSelectorItemChild {
-  const FileSelectorItemImageGallery(
-      {super.key,
-      required super.onChanged,
-      super.icon = CupertinoIcons.photo,
-      super.label = "Choisir une photo",
-      super.bordered = true});
+  const FileSelectorItemImageGallery({
+    super.key,
+    required super.onChanged,
+    super.icon = CupertinoIcons.photo,
+    super.label = "Choisir une photo",
+    super.bordered = true,
+    super.type = FileSelectorItemType.image,
+    super.source,
+  });
 }
